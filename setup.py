163c6b4d16f47cf0b201ab3c5febdee4cfaef3d9
Roger Caldwell <rocaldwe@cisco.com>
2015-03-10 11:48:55 -0500

    updates

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='cisco_pass',
-    packages=['cisco_pass', 'passlib'],
+    packages=['cisco_pass'],
     version='0.1',
     description='Generate Cisco password strings.  Decrypt type7.  Devices: IOS, NXOS, ACE, CatOS',
     author='Roger Caldwell',
@@ -10,5 +11,6 @@ setup(
     url='https://github.com/rcaldwel/cisco_pass',
     download_url='https://github.com/rcaldwel/cisco_pass/tarball/0.1',
     keywords=['cisco', 'password', 'crypt'],
+    install_requires=['passlib'],
     classifiers=[],
 )
